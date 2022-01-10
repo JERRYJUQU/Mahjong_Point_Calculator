0c55c638873c77beefd5710769a27b02e3a4821b
JERRYJUQU <jerryjuqu@gmail.com>
2022-01-09 21:26:11 -0500

    Update Calculator.kt

--- Calculator/src/main/kotlin/Calculator.kt
+++ Calculator/src/main/kotlin/Calculator.kt
@@ -1,9 +1,17 @@
 package src.main
 
+import java.util.*
+
 class Calculator {
+    val score = ScoringTypes()
+
+    fun hasBigFourWinds(meld : Vector<Meld>) : Boolean{
+
+    }
+
     fun calculate (hand : Hand) : Void {
-        if (hasBigFourWinds(hand.melds)) {
-            hand.points +=
+        if (this.hasBigFourWinds(hand.melds)) {
+            hand.points +=score
         }
         if (bigThreeDragons(hand.melds))
     }
