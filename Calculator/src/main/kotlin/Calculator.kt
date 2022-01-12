1f7d99097b08b74d62a856162e5fd643344fed4e
JERRYJUQU <jerryjuqu@gmail.com>
2022-01-11 20:50:28 -0500

    Update Calculator and Hand

--- Calculator/src/main/kotlin/Calculator.kt
+++ Calculator/src/main/kotlin/Calculator.kt
@@ -5,14 +5,7 @@ import java.util.*
 class Calculator {
     val score = ScoringTypes()
 
-    fun hasBigFourWinds(meld : Vector<Meld>) : Boolean{
-
-    }
-
     fun calculate (hand : Hand) : Void {
-        if (this.hasBigFourWinds(hand.melds)) {
-            hand.points +=score
-        }
-        if (bigThreeDragons(hand.melds))
+
     }
 }
--- Calculator/src/main/kotlin/Hand.kt
+++ Calculator/src/main/kotlin/Hand.kt
@@ -5,4 +5,8 @@ import java.util.*
 class Hand(temp_melds : Vector<Meld>){
     var melds = temp_melds
     var points = 0
+
+    fun bigFourWinds() : Boolean{
+        
+    }
 }
